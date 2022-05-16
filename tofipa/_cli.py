e1b3e65615a02cd509d5965ec143f07227491d6c
plotski <plotski@example.org>
2022-05-16 18:00:13 +0200

    _cli.py: Read list of download locations from config file

--- tofipa/_cli.py
+++ tofipa/_cli.py
@@ -1,9 +1,8 @@
 import argparse
 import sys
 
-from . import __description__, __project_name__, __version__
-from ._errors import FindError
-from ._location import FindDownloadLocation
+from . import (__description__, __project_name__, __version__, _config,
+               _errors, _location)
 
 
 def _parse_args(args):
@@ -17,8 +16,13 @@ def _parse_args(args):
     )
     argparser.add_argument(
         'LOCATION',
-        nargs='+',
+        nargs='*',
         help='Potential path of existing files in TORRENT',
+    )
+    argparser.add_argument(
+        '--locations-file', '--lf',
+        default=_config.DEFAULT_LOCATIONS_FILEPATH,
+        help='File containing newline-separated list of download locations',
     )
     argparser.add_argument(
         '--default',
@@ -36,23 +40,39 @@ def _parse_args(args):
     return argparser.parse_args(args)
 
 
+def _fatal_error(msg):
+    sys.stderr.write(f'{msg}\n')
+    sys.exit(1)
+
+
 def cli(args=None):
     if args is None:
         args = sys.argv[1:]
     args = _parse_args(args)
 
+    # Debugging
     if args.debug_file:
         import logging
         logging.basicConfig(filename=args.debug_file, level=logging.DEBUG)
 
-    location_finder = FindDownloadLocation(
+    # Read locations file
+    try:
+        locations = _config.LocationsFile(args.locations_file)
+    except _errors.ConfigError as e:
+        _fatal_error(e)
+    else:
+        if not locations:
+            _fatal_error(f'No locations specified. See: {__project_name__} --help')
+
+    # Find location for torrent
+    location_finder = _location.FindDownloadLocation(
         torrent=args.TORRENT,
-        locations=args.LOCATION,
+        locations=locations,
         default=args.default,
     )
     try:
         download_location = location_finder.find()
-    except FindError as e:
+    except _errors.FindError as e:
         sys.stderr.write(f'{e}\n')
     else:
         sys.stdout.write(download_location)
