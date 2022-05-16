5fa7c3b06b4d6ed9e76538092579815f1c12bfeb
plotski <plotski@example.org>
2022-05-16 17:56:28 +0200

    Add ConfigError

--- tofipa/_errors.py
+++ tofipa/_errors.py
@@ -1,2 +1,12 @@
 class FindError(Exception):
     """Raised by :class:`~.FindDownloadLocation` and its methods"""
+
+
+class ConfigError(Exception):
+    """Raised by configuration file readers"""
+
+    def __init__(self, msg, filepath=None, line_number=None):
+        if filepath and line_number:
+            super().__init__(f'{filepath}@{line_number}: {msg}')
+        else:
+            super().__init__(msg)
